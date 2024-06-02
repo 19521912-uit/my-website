2fd2403a40342d6f6dcd8af7d7b139aef5eafb5c
19521912-uit <66859973+19521912-uit@users.noreply.github.com>
2024-06-02 12:49:30 +0700

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/database";
+import firebase from "./firebase/app";
+import "./firebase/database";
 
 // Cấu hình Firebase của bạn
 const firebaseConfig = {
